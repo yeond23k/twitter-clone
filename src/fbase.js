626b86d87fa55a85d02e4a609ff256ceab6b072a
Daryl <dyk0203@naver.com>
2022-12-21 01:10:13 +0900

    3.0 Form and Database Setup

--- src/fbase.js
+++ src/fbase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import "firebase/compat/database";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,34 @@
-const Home = () => <span>Home</span>;
+import { useState } from "react";
+
+const Home = () => {
+  const [twit, setTwit] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
+    setTwit(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={twit}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Twit" />
+      </form>
+    </div>
+  );
+};
 
 export default Home;
