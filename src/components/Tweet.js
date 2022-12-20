1c30980b62224f3a9ac0b6d8fe20d5560aaca1ac
Daryl <dyk0203@naver.com>
2022-12-21 01:52:13 +0900

    3.4 Delete and Update part One

---
+++ src/components/Tweet.js
@@ -0,0 +1,15 @@
+const Tweet = ({ tweetObj, isOwner }) => {
+  return (
+    <div>
+      <h4>{tweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button>Delete Tweet</button>
+          <button>Edit Tweet</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Tweet;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,4 @@
+import Tweet from "components/Tweet";
 import { dbService } from "fbase";
 import { useEffect, useState } from "react";
 
@@ -48,9 +49,11 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {tweets.map((tweet) => (
-          <div key={tweet.id}>
-            <h4>{tweet.text}</h4>
-          </div>
+          <Tweet
+            key={tweet.id}
+            tweetObj={tweet}
+            isOwner={tweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
