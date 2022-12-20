55f766977e3e28a61d3fda731c13bccdb6cbfc5e
Daryl <dyk0203@naver.com>
2022-12-21 02:09:19 +0900

    3.5 Delete and Update part Two

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,11 +1,56 @@
+import { dbService } from "fbase";
+import { useState } from "react";
+
 const Tweet = ({ tweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newTweet, setNewTweet] = useState(tweetObj.text);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("Are you sure you want to delete this tweet?");
+    if (ok) {
+      await dbService.doc(`tweets/${tweetObj.id}`).delete();
+    }
+  };
+
+  const toggleEditing = () => setEditing((prev) => !prev);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`tweets/${tweetObj.id}`).update({
+      text: newTweet,
+    });
+    setEditing(false);
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewTweet(value);
+  };
+
   return (
     <div>
-      <h4>{tweetObj.text}</h4>
-      {isOwner && (
+      {editing ? (
         <>
-          <button>Delete Tweet</button>
-          <button>Edit Tweet</button>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Edit your tweet"
+              value={newTweet}
+              required
+              onChange={onChange}
+            />
+            <input type="submit" value="Update Tweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{tweetObj.text}</h4>
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Tweet</button>
+              <button onClick={toggleEditing}>Edit Tweet</button>
+            </>
+          )}
         </>
       )}
     </div>
