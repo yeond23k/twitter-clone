c9c3235e80bc0850cc32a207b59fb4a5ac0b006b
Daryl <dyk0203@naver.com>
2022-12-22 10:28:49 +0900

    4.0 Preview Images part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -35,6 +35,18 @@ const Home = ({ userObj }) => {
     setTweet(value);
   };
 
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
+    };
+    reader.readAsDataURL(theFile);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -45,6 +57,7 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Tweet" />
       </form>
       <div>
